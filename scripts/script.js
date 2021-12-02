fe3b5f68b3aaf85afcc718a2bd008b03aab87cbb
Sofie Berger <94051947+SofieBerger@users.noreply.github.com>
2021-12-02 20:46:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,5 @@
 // JavaScript Document
 
-// Dark mode
-// const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-// function switchTheme(e) {
-//     if (e.target.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//     }
-//     else {
-//         document.documentElement.setAttribute('data-theme', 'light');
-//     }    
-// }
-
-// toggleSwitch.addEventListener('change', switchTheme, false);
-
-
 // hamburger menu
 var dropdownMenu = document.querySelector ("nav");
 
